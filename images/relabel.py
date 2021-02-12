1a353d986ef1c3854d9267ef3766f4a9c8c6a1ca
William <william1504@gmail.com>
2021-02-11 22:00:15 -0300

    dataset

---
+++ images/relabel.py
@@ -0,0 +1,28 @@
+import os
+import json
+import glob
+
+base_dir = './images/'
+
+labels = [
+    'ardido',
+    'brocado',
+    'marinheiro',
+    'normal',
+    'preto',
+    'verde'
+]
+
+for label in labels:
+    print(f'Loading data from: {label}')
+
+    addrs = glob.glob(os.path.join(base_dir + label, '*.json'))
+    for addr in addrs:
+        with open(addr, 'r+') as json_file:
+            data = json.load(json_file)
+
+        for shape in data['shapes']:
+            shape['label'] = label
+
+        with open(addr, 'w+') as f:
+            json.dump(data, f, indent=2)
